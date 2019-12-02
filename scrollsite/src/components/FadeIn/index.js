c564443eda6721c2c98e2cf78ea5eff53d6727b1
Justin Pinero <justinpinero@gmail.com>
2019-12-02 00:50:16 -0500

    first

---
+++ scrollsite/src/components/FadeIn/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FadeIn() {
+  return (
+    <div>
+   
+    </div>
+  );
+}
+
+export default FadeIn;
---
+++ scrollsite/src/components/Zoom/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Zoom() {
+  return (
+    <div>
+   
+    </div>
+  );
+}
+
+export default Zoom;
