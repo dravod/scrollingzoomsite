e288eef460a35d597196d0f586b34d47d9052939
Justin Pinero <justinpinero@gmail.com>
2019-12-02 12:52:32 -0500

    added public files

--- scrollsite/src/App.js
+++ scrollsite/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Routes from "./Routes";
+import posed from 'react-pose';
 
 function App() {
   return (
---
+++ scrollsite/src/containers/HomeScreen.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Component} from 'react'
+import posed from 'react-pose';
+
+export default class  extends Component.Homescreen {
+    state = {  }
+    const Box = posed.div({
+        hidden: { opacity: 0 },
+        visible: { opacity: 1 }
+      });
+    render() { 
+        return ( 
+
+
+         );
+    }
+}
